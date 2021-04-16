b7ef6cda6e4871c27fb19e9f9b2e7f2ec9e4c371
Aaron Lei <aaron.l@live.ca>
2021-04-16 15:28:57 -0400

    fixed card position on mobile

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,16 +4,18 @@ import "./Card.css";
 
 function Card() {
   return (
-    <div className="cards">
-      <div className="cards__container">
-        <div className="cards__wrapper">
-          <ul className="cards__items">
-            <CardItem
-              src="images/finances.jpg"
-              label="Monthly Budgeting App"
-              path="https://github.com/aaronl647/monthlyBudgetApp"
-            />
-          </ul>
+    <div className="card-container">
+      <div className="cards">
+        <div className="cards__container">
+          <div className="cards__wrapper">
+            <ul className="cards__items">
+              <CardItem
+                src="images/finances.jpg"
+                label="Monthly Budgeting App"
+                path="https://github.com/aaronl647/monthlyBudgetApp"
+              />
+            </ul>
+          </div>
         </div>
       </div>
     </div>
