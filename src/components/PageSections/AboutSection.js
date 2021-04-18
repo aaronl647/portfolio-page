aee8c37e6a44a4bebb41fb211011e52bc88366a4
Aaron Lei <aaron.l@live.ca>
2021-04-18 17:17:06 -0400

    updated aboutsection css

--- src/components/PageSections/AboutSection.js
+++ src/components/PageSections/AboutSection.js
@@ -8,30 +8,32 @@ function AboutSection() {
       <Container className="section-container">
         <Row>
           <Col col={4} className="about-container">
-            <h3>Hey there,</h3>
-            <br />
-            <p className="about-content">
-              My name is Aaron and I'm a software engineer.
+            <div className="about-wrapper">
+              <h3>Hey there,</h3>
               <br />
-              <br />
-              I'm a recent software engineering graduate from General Assembly.
-              I decided to pursue a career in software because first and
-              foremost I've always had a passion for anything technology and
-              software related.
-              <br />
-              <br />
-              I also want to make a meaningful impact on the world and I figured
-              the best way to do that would be to build new and exciting
-              technologies to help people in need.
-              <br />
-              <br />
-              Prior to the career pivot, I had studied Event and Media
-              Production at Seneca College and worked in the industry for over 5
-              years.
-              <br />
-              <br />
-              <div className="sign-off">- Aaron Lei</div>
-            </p>
+              <p className="about-content">
+                My name is Aaron and I'm a software engineer.
+                <br />
+                <br />
+                I'm a recent software engineering graduate from General
+                Assembly. I decided to pursue a career in software because first
+                and foremost I've always had a passion for anything technology
+                and software related.
+                <br />
+                <br />
+                I also want to make a meaningful impact on the world and I
+                figured the best way to do that would be to build new and
+                exciting technologies to help people in need.
+                <br />
+                <br />
+                Prior to the career pivot, I had studied Event and Media
+                Production at Seneca College and worked in the industry for over
+                5 years.
+                <br />
+                <br />
+                <div className="sign-off">- Aaron Lei</div>
+              </p>
+            </div>
           </Col>
           <Col sm={8} className="profile-container">
             <img
